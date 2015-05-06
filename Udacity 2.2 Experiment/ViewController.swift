c11a4847c91dc136a7d789869241efdbf0b8c283
Brendan McDonald <brendan@brendandmcdonald.com>
2015-05-06 12:15:54 +0100

    all three experiments worked

--- Udacity 2.2 Experiment/ViewController.swift
+++ Udacity 2.2 Experiment/ViewController.swift
@@ -25,12 +25,36 @@ class ViewController: UIViewController {
 
     @IBAction func loadNextScreen(){
         
+        //Exercise 3: Alert view
+        
+        let controller = UIAlertController(title: "Alert", message: "Hello, World", preferredStyle: .Alert)
+        // you could also define the UIAlter controller title and method inline instead of initializing the controller variable with it.
+        // That would look like this:
+        // controller.title = "Happy"
+        // controller.message = "Hello, World"
+        
+        /* the below doesn't work but I don't understand why:
+
+        let OkAction = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Default, handler: nil){action in self.dismissViewControllerAnimated(true, completion: nil)}
+
+        */
+        
+        let okAction = UIAlertAction(title: "Got it", style: UIAlertActionStyle.Default){action in self.dismissViewControllerAnimated(true, completion: nil)}
+        
+        controller.addAction(okAction)
+        
+        self.presentViewController(controller, animated: true, completion: nil)
+        
+        
+        /* Excercise 2: Activity View
+        
         let image = UIImage()
         let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
         
         self.presentViewController(controller, animated: true, completion: nil)
+        */
         
-        /* initial exercise where we accessed the camera picker
+        /* Exercise 1: Image Picker
 
         let nextViewController = UIImagePickerController()
         
