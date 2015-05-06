f91c9545b03c6eea1ffbd9a73e4170d98c22facb
Brendan McDonald <brendan@brendandmcdonald.com>
2015-05-06 11:16:12 +0100

    Initial Commit

---
+++ Udacity 2.2 Experiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udacity 2.2 Experiment
+//
+//  Created by Brendan McDonald on 5/6/15.
+//  Copyright (c) 2015 Dangerous Design LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Udacity 2.2 ExperimentTests/Udacity_2_2_ExperimentTests.swift
@@ -0,0 +1,36 @@
+//
+//  Udacity_2_2_ExperimentTests.swift
+//  Udacity 2.2 ExperimentTests
+//
+//  Created by Brendan McDonald on 5/6/15.
+//  Copyright (c) 2015 Dangerous Design LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Udacity_2_2_ExperimentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
